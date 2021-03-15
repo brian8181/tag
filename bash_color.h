86843710c536436d156fe261249d7270f6b46480
Brian K <brian8181@gmail.com>
2021-03-15 18:36:44 -0500

    add copied files

---
+++ bash_color.h
@@ -0,0 +1,30 @@
+#ifndef _BASH_COLOR_HPP
+#define _BASH_COLOR_HPP
+
+#include <iostream>
+
+//using std::string;
+
+// shell color constants
+const string FMT_FG_DEFUALT        = "\033[39m";
+const string FMT_FG_RED            = "\033[31m";
+const string FMT_FG_GREEN          = "\033[32m";
+const string FMT_FG_YELLOW         = "\033[33m";
+const string FMT_FG_BLUE           = "\033[34m";
+const string FMT_FG_MAGENTA        = "\033[35m";
+const string FMT_FG_CYAN           = "\033[36m";
+const string FMT_FG_LIGHT_GREY     = "\033[37m";
+const string FMT_FG_DARK_GREY      = "\033[90m";
+const string FMT_FG_LIGHT_RED      = "\033[91m";
+const string FMT_FG__LIGHT_GREEN   = "\033[92m";
+const string FMT_FG__LIGHT_YELLOW  = "\033[93m";
+const string FMT_FG__LIGHT_BLUE    = "\033[94m";
+const string FMT_FG__LIGHT_MAGENTA = "\033[95m";
+const string FMT_FG__LIGHT_CYAN    = "\033[96m";
+const string FMT_FG_WHITE          = "\033[97m";
+const string FMT_UNDERLINE         = "\033[4m";
+const string FMT_BOLD              = "\033[1m";
+const string FMT_RESET             = "\033[0m";
+
+#endif
+
---
+++ tag.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <getopt.h>
+#include "bash_color.h"
+
+using namespace std;
+
+static struct option long_options[] =
+{
+    {"verbose", no_argument, 0, 'v'},
+    {"help", no_argument, 0, 'h'},
+    {"file", no_argument, 0, 'f'}
+};
+
+/*
+
+ tag [opts] [INPUT] ... ]
+
+*/
+
+const int DEFAULT_ARGC = 3;
+
+int main(int argc, char *argv[])
+{
+    int opt = 0;
+    int option_index = 0;
+    bool file_flag = false;
+    bool verbose_flag = false;
+    
+    optind = 0;
+    while((opt = getopt_long(argc, argv, "hvf", long_options, &option_index)) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            print_help();
+            return 0;
+        case 'v':
+            verbose_flag = true;
+            break;
+        case 'f':
+            file_flag = true;
+            break;
+        default: // unknown option before args
+            fprintf(stderr, "Unexpected option, -h for help\n");
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (optind != (argc - DEFAULT_ARGC)) // not correct number of args
+    {
+        fprintf(stderr, "Expected argument after options, -h for help\n");
+        exit(EXIT_FAILURE);
+    }
+          
+    if (verbose_flag)
+    {
+        print_help();
+    }
+}
+
+void print_help()
+{
+    cout << "\n"
+         << FMT_BOLD << "scanpf" << FMT_RESET << " "
+         << "[OPTIONS] "
+         << FMT_UNDERLINE << "INPUT_PATTERN" << FMT_RESET << " "
+         << FMT_UNDERLINE << "OUTPUT_PATTERN" << FMT_RESET << " "
+         << FMT_UNDERLINE << "[INPUT ...]" << FMT_RESET << "\n\n";
+}
